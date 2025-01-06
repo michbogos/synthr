16dad7eeaeb349c42a20af60a233b243d26bbfb9
michbogos <bogostamicheal@gmail.com>
2025-01-06 21:12:41 +0100

    Fix build

--- gui/main.cpp
+++ gui/main.cpp
@@ -1,6 +1,6 @@
-#include "imgui/imgui.h"
-#include "imgui/backends/imgui_impl_glfw.h"
-#include "imgui/backends/imgui_impl_opengl3.h"
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
 #include "imnodes.h"
 #include <stdio.h>
 #include <vector>
