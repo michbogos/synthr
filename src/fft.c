cb2ec4713b6cdf314a6ae5a6761e4358ace1be95
michbogos <bogostamicheal@gmail.com>
2025-01-17 22:06:07 +0100

    Remove reverse

--- src/fft.c
+++ src/fft.c
@@ -1,33 +1,27 @@
 #include<fft.h>
 
-int reverse(int num, int lg_n) {
-    int res = 0;
-    for (int i = 0; i < lg_n; i++) {
-        if (num & (1 << i))
-            res |= 1 << (lg_n - 1 - i);
-    }
-    return res;
-}
-
-void fft(complex* x, int N){
+void fft(complex* x, int N, int invert){
     int log_n = 0;
     while((1<<log_n) < N){
         log_n ++;
     }
-    for(int i = 0; i < N; i++){
-        int rev = reverse(i, log_n);
-        if(i < rev){
-            float tmp = x[i];
-            x[i] = x[rev];
-            x[rev] = tmp;
+    for (int i = 1, j = 0; i < N; i++) {
+        int bit = N >> 1;
+        for (; j & bit; bit >>= 1)
+            j ^= bit;
+        j ^= bit;
+        if (i < j){
+            complex tmp = x[i];
+            x[i] = x[j];
+            x[j] = tmp;
         }
     }
     for (int len = 2; len <= N; len <<= 1) {
-        double ang = 2 * PI / len;
+        double ang = 2 * PI / len * (invert ? -1 : 1);
         complex wlen = cos(ang)+I*sin(ang);
         for (int i = 0; i < N; i += len) {
             complex w = 1.0;
-            for (int j = 0; j < len / 2; j++) {
+            for (int j = 0; j < len / 2; j++){
                 complex u = x[i+j];
                 complex v = x[i+j+len/2] * w;
                 x[i+j] = u + v;
@@ -35,5 +29,10 @@ void fft(complex* x, int N){
                 w *= wlen;
             }
         }
+    }
+
+    if (invert) {
+        for(int i = 0; i < N; i++)
+            x[i] /= N;
     }
 }
