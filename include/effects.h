353d681ebbd2c155c4a1180f24c4e2262561b897
michbogos <bogostamicheal@gmail.com>
2025-02-15 22:10:33 +0100

    Add bitcrusher effect

--- include/effects.h
+++ include/effects.h
@@ -3,8 +3,7 @@
 
 #include<math.h>
 
-float distortion_foldback(float in, float threshold)
-{
+float distortion_foldback(float in, float threshold){
   while (in > threshold || in < -threshold)
   {
     in = fabsf(fabsf(fmodf(in - threshold, threshold*4)) - threshold*2) - threshold;
@@ -12,9 +11,12 @@ float distortion_foldback(float in, float threshold)
   return in;
 }
 
-float distortion_exp(float x)
-{
+float distortion_exp(float x){
   return x/fabsf(x)*(1.0f-exp(-powf(x,2)/fabsf(x)));
+}
+
+float bit_crusher(float x, float bits){
+  return ((int)(x*powf(2, bits)))/powf(2, bits);
 }
 
 #endif
